cadca7c4318568b1e393b76b9c39afe688012d15
Ahmed Faiza <jesminnakter01@gmail.com>
2025-09-16 21:34:19 +0600

    Updated CampusCrave project

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Banner from "../../components/Banner/Banner";
+import OurHomeMenu from "../../components/OurHomeMenu/OurHomeMenu";
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <OurHomeMenu />
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/MealPlans/MealPlans.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const MealPlans = () => {
+  return (
+    <div>
+          <>
+      <Navbar/>
+    </>
+    </div>
+  )
+}
+
+export default MealPlans
+
