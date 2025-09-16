a60185768f42dc82bf06d28098a990be79ecca23
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-09-16 21:47:46 +0600

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Banner from "../../components/Banner/Banner";
+import OurHomeMenu from "../../components/OurHomeMenu/OurHomeMenu";
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <OurHomeMenu />
+    </>
+  );
+};
+
+export default Home;
 
