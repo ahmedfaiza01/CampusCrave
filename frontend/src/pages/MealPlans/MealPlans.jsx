f7dd97e0f0593d0e16f74e4c2965676d989d3d90
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-10-30 15:45:27 +0600

    Update MealPlans.jsx

--- frontend/src/pages/MealPlans/MealPlans.jsx
+++ frontend/src/pages/MealPlans/MealPlans.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import SubscriptionForm from '../../components/MealPlan/Subscription'
+import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
+import Subscription from '../../components/MealPlan/Subscription'
 
 const MealPlans = () => {
   return (
-    <div>
-          <>
-      <Navbar/>
+     <>
+       <Navbar/>
+       <Subscription/>
+       <Footer/>
     </>
-    </div>
   )
 }
 
 export default MealPlans
-
