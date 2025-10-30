80e79c23734c62b987cfb1bc5b05a624d1d2e375
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-10-30 15:43:57 +0600

    Create MealPlan.jsx

---
+++ frontend/src/pages/MealPlans/MealPlan.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SubscriptionForm from '../../components/MealPlan/Subscription'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+import Subscription from '../../components/MealPlan/Subscription'
+
+const MealPlans = () => {
+  return (
+     <>
+       <Navbar/>
+       <Subscription/>
+       <Footer/>
+    </>
+  )
+}
+
+export default MealPlans
