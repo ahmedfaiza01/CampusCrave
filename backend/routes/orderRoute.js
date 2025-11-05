911d1ae287d5c9ac43f7cabd6b45e1f74f80bb3e
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-11-06 00:31:01 +0600

    Add files via upload

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+import { confirmPayment, createOrder, getAllOrders, getOrderById, getOrders, updateAnyOrder, updateOrder } from '../controllers/orderController.js'
+import authMiddleware from '../middleware/auth.js'
+
+const orderRouter = express.Router()
+
+orderRouter.get('/getall', getAllOrders);
+orderRouter.put('/getall/:id', updateAnyOrder);
+
+// PROTECT REST OF ROUTES USING MIDDLEWARE
+orderRouter.use(authMiddleware)
+
+orderRouter.post('/', createOrder);
+orderRouter.get('/', getOrders);
+orderRouter.get('/confirm', confirmPayment);
+orderRouter.get('/:id', getOrderById);
+orderRouter.put('/:id', updateOrder);
+
+export default orderRouter
