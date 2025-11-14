d1ed7dcb2f66d4c882e6576668a5638a61646f55
ahmedfaiza01 <146230491+ahmedfaiza01@users.noreply.github.com>
2025-11-14 14:11:15 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import ContactPage from "./pages/ContactPage/ContactPage";
 import Menu from "./pages/Menu/Menu";
 import Cart from "./pages/Cart/Cart";
 import MealPlans from "./pages/MealPlans/MealPlans"; 
-
+ 
 import SignUp from "./components/SignUp/SignUp";
 import Aboutpage from "./pages/Aboutpage/Aboutpage";
 
-
-
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import VerifyPaymentPage from "./pages/VerifyPaymentPage/VerifyPaymentPage";
+import MyOrderPage from "./pages/MyOrderPage/MyOrderPage";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 
 const App = () => {
   return (
@@ -19,12 +21,16 @@ const App = () => {
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/about" element={<Aboutpage />} />
       <Route path="/menu" element={<Menu />} />
-      <Route path="/cart" element={<Cart />} />
       <Route path="/mealplans" element={<MealPlans />} />
-  
-       <Route path="/login" element={<Home />} />
-       <Route path="/SignUp" element={<SignUp />} />
       
+      <Route path="/login" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/myorder/verify" element={<VerifyPaymentPage />} />
+
+      {/* ✅ Protected Routes */}
+      <Route path="/cart" element={<PrivateRoute><Cart /></PrivateRoute>} />
+      <Route path="/checkout" element={<PrivateRoute><CheckoutPage /></PrivateRoute>} />
+      <Route path="/myorder" element={<PrivateRoute><MyOrderPage /></PrivateRoute>} />
     </Routes>
   );
 };
